137f628e6ee462b7e15a4c7529826ab56b9bb0bd
dmm4613 <dmm4613@gmail.com>
2020-10-07 12:55:53 -0400

    Updates

---
+++ precache-manifest.018c834ddf427b1a918cd5f18398eb83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c6f0b409fc89f25f98d47e04b2d3cb",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "58e8aa49646077f5e32b",
+    "url": "/react-todo/static/css/main.75c22403.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/react-todo/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e8aa49646077f5e32b",
+    "url": "/react-todo/static/js/main.61882c6a.chunk.js"
+  },
+  {
+    "revision": "99707064901b90cc2286",
+    "url": "/react-todo/static/js/runtime-main.e03cb3ff.js"
+  }
+]);
